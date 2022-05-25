d023e403c902a0419f75d702bf32705fa0026c27
denvoiten <den.voiten@gmail.com>
2022-05-25 15:07:20 +0300

    2. Тестовое задание - проект "Pooh JMS" [#268841]

---
+++ src/test/java/ru/job4j/pooh/QueueServiceTest.java
@@ -0,0 +1,23 @@
+package ru.job4j.pooh;
+
+import org.junit.Ignore;
+import org.junit.Test;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
+
+public class QueueServiceTest {
+    @Ignore
+    @Test
+    public void whenPostThenGetQueue() {
+        QueueService queueService = new QueueService();
+        String paramForPostMethod = "temperature=18";
+        queueService.process(
+                new Req("POST", "queue", "weather", paramForPostMethod)
+        );
+        Resp result = queueService.process(
+                new Req("GET", "queue", "weather", null)
+        );
+        assertThat(result.text(), is("temperature=18"));
+    }
+}
