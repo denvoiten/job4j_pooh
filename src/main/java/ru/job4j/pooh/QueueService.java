7c307544f4cdf256510513443d110a4e4e57affd
denvoiten <den.voiten@gmail.com>
2022-05-28 19:36:16 +0300

    2. Тестовое задание - проект "Pooh JMS" [#268841 #305551]

--- src/main/java/ru/job4j/pooh/QueueService.java
+++ src/main/java/ru/job4j/pooh/QueueService.java
@@ -11,12 +11,13 @@ public class QueueService implements Service {
     public Resp process(Req req) {
         Resp resp = new Resp(req.httpRequestType(), "501 Not Implemented");
         String param = req.getParam();
+        String sourceName = req.getSourceName();
         if ("POST".equals(req.httpRequestType())) {
-            queues.putIfAbsent(req.getSourceName(), new ConcurrentLinkedQueue<>());
-            queues.get(req.getSourceName()).offer(param);
+            queues.putIfAbsent(sourceName, new ConcurrentLinkedQueue<>());
+            queues.get(sourceName).offer(param);
             resp = param.isEmpty() ? new Resp(param, "200 Ok") : new Resp(param, "201 Created");
         } else if ("GET".equals(req.httpRequestType())) {
-            String text = queues.getOrDefault(req.getSourceName(), new ConcurrentLinkedQueue<>()).poll();
+            String text = queues.getOrDefault(sourceName, new ConcurrentLinkedQueue<>()).poll();
             resp = text == null ? new Resp("", "204 No Content") : new Resp(text, "200 Ok");
         }
         return resp;
